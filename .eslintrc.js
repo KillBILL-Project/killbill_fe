1f791ff9ba14016447d91def5415d7b623871e38
bumja <gngk1234@naver.com>
2023-05-28 14:11:08 +0900

    init-setting dependency 설치 및 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,46 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    browser: true,
+    es2021: true,
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: './tsconfig.json',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
+  rules: {
+    // indent: ['error', 2, { SwitchCase: 1 }],
+    quotes: ['error', 'single', { avoidEscape: true }],
+    semi: ['error', 'always'],
+    'prettier/prettier': 'error',
+    'linebreak-style': ['error', 'unix'],
+    'no-empty-function': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    'react/display-name': 'off',
+    'react/prop-types': 'off',
+    indent: 'off',
+    '@typescript-eslint/indent': ['error', 2, { SwitchCase: 1 }],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 };
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,23 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@': './src',
+          '@components': './src/components',
+          '@apis': './src/apis',
+          '@assets': './src/assets',
+          '@common': './src/common',
+          '@enum': './src/enum',
+          '@queries': './src/queries',
+          '@hooks': './src/hooks',
+          '@styles': './src/styles',
+          '@navigation': './src/navigation',
+        },
+      },
+    ],
+  ],
 };
