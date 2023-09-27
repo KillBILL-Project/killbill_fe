866d773e4a65b8f840c754f5de675d85ee703fcc
bumja <gngk1234@naver.com>
2023-09-27 14:44:51 +0900

    react navigation 설치

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Text } from 'react-native';
 import styled from 'styled-components/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
 
 const Main = styled.SafeAreaView`
   flex: 1;
@@ -8,11 +10,27 @@ const Main = styled.SafeAreaView`
   align-items: center;
 `;
 
-const App = () => {
+const Home = () => {
   return (
     <Main>
       <Text>안녕하세요</Text>
     </Main>
+  );
+};
+
+type RootStackParamList = {
+  Home: undefined;
+};
+
+const Stack = createStackNavigator<RootStackParamList>();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen options={{ headerShown: false }} name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
