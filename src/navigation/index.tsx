62896d4ac62eed5b6dbf09a726663484289673be
bumja <gngk1234@naver.com>
2024-02-13 15:08:09 +0900

    getUser 분리, 의존성 수정

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,13 +1,16 @@
 import React, { useEffect } from 'react';
 import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
-import { useRecoilState, useRecoilValue } from 'recoil';
+import { useRecoilValue } from 'recoil';
 import AuthNavigation from './AuthNavigation';
 import HomeNavigation from './HomeNavigation';
-import { tokenState, userState } from '../states';
+import { tokenState } from '../states';
 import AppFrame from '../components/common/AppFrame';
-import { getUserInfo } from '../services/api/authService';
+import useAuth from '../hooks/useAuth';
 
 const Navigation = () => {
+  const accessToken = useRecoilValue(tokenState);
+  const { getUser } = useAuth();
+
   const MyTheme = {
     ...DefaultTheme,
     colors: {
@@ -15,28 +18,12 @@ const Navigation = () => {
       background: 'transparent',
     },
   };
-  const accessToken = useRecoilValue(tokenState);
-  const [user, setUser] = useRecoilState(userState);
 
   useEffect(() => {
-    if (!accessToken && !user) return;
-
-    const getUser = async () => {
-      const response = await getUserInfo();
-      const userInfo = response.data.data;
-      if (!userInfo) {
-        // TODO: 유저정보가 없을리 없지만 없다면 로그인화면으로 보내야하나?
-      }
-      setUser({
-        email: userInfo.email,
-        country: userInfo.country,
-        loginType: userInfo.loginType,
-        pushConsent: userInfo.pushConsent,
-      });
-    };
-
+    if (!accessToken) return;
     getUser();
-  }, [accessToken, setUser, user]);
+  }, [accessToken]);
+
   return (
     <NavigationContainer theme={MyTheme}>
       <AppFrame>{accessToken ? <HomeNavigation /> : <AuthNavigation />}</AppFrame>
