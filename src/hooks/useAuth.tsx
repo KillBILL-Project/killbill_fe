01d821ceddba68ee0c0d4443f19939e80e6c140b
bumja <gngk1234@naver.com>
2024-02-14 20:23:22 +0900

    유저정보 받아오는 함수 공통으로 분리

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,15 +1,18 @@
 import { useSetRecoilState } from 'recoil';
-import { tokenState } from '../states';
+import { tokenState, userState } from '../states';
 import { removeRefreshToken, saveRefreshToken } from '../services/storage/encryptedStorage';
 import { LoginResponse } from '../types/auth';
+import { getUserInfo } from '../services/api/authService';
 
 interface UseAuthType {
   setTokens: (params: LoginResponse) => Promise<void>;
   clearTokens: () => Promise<void>;
+  getUser: () => Promise<void>;
 }
 
 const useAuth = (): UseAuthType => {
   const setAccessToken = useSetRecoilState(tokenState);
+  const setUser = useSetRecoilState(userState);
 
   const setTokens = async (params: LoginResponse) => {
     setAccessToken(params.accessToken ?? null);
@@ -21,7 +24,16 @@ const useAuth = (): UseAuthType => {
     await removeRefreshToken();
   };
 
-  return { setTokens, clearTokens };
+  const getUser = async () => {
+    const response = await getUserInfo();
+    const userInfo = response.data.data;
+    if (!userInfo) {
+      // TODO: 유저정보가 없을리 없지만 없다면 로그인화면으로 보내야하나?
+    }
+    setUser({ ...userInfo });
+  };
+
+  return { setTokens, clearTokens, getUser };
 };
 
 export default useAuth;
