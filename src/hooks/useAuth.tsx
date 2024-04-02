8de643474e239a3a27d589a5aa05da8b376feb0f
bumja <gngk1234@naver.com>
2024-04-02 10:46:06 +0900

    fix: apiClient null 경고 발생하기도 하고, 어차피 필요 없는 것 같아서 제거

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,5 +1,4 @@
 import { useSetRecoilState } from 'recoil';
-import { GoogleSignin } from '@react-native-google-signin/google-signin';
 import { tokenState, userState } from '../states';
 import { removeRefreshToken, saveRefreshToken } from '../services/storage/encryptedStorage';
 import { LoginResponse } from '../types/auth';
@@ -26,21 +25,16 @@ const useAuth = (): UseAuthType => {
     await removeRefreshToken();
   };
 
-  const getUser = async () => {
-    const response = await getUserInfo();
-    const userInfo = response.data.data;
-    if (!userInfo) {
-      // TODO: 유저정보가 없을리 없지만 없다면 로그인화면으로 보내야하나?
-    }
-    setUser({ ...userInfo });
-  };
-
   const logout = async () => {
     setUser(null);
     await clearTokens();
-    if (await GoogleSignin.isSignedIn()) {
-      await GoogleSignin.signOut();
-    }
+  };
+
+  const getUser = async () => {
+    const response = await getUserInfo();
+    const userInfo = response.data.data;
+
+    setUser({ ...userInfo });
   };
 
   return { setTokens, clearTokens, getUser, logout };
