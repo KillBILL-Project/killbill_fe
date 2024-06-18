59189eaadbb2e6e1e0bdfb44ec0ba16cbcc662e1
bumja <gngk1234@naver.com>
2024-06-18 17:25:54 +0900

    path alias 적용

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,25 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: ['react-native-reanimated/plugin'],
+  plugins: [
+    'react-native-reanimated/plugin',
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@constants': './src/constants',
+          '@domains': './src/domains',
+          '@hooks': './src/hooks',
+          '@locales': './src/locales',
+          '@navigation': './src/navigation',
+          '@services': './src/services',
+          '@states': './src/states',
+          '@types': './src/types',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+  ],
 };
