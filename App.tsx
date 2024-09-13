e9dc966bfd5ae6594fb26d2a6534f2882205db22
bumja <gngk1234@naver.com>
2024-09-13 10:24:56 +0900

    fix: changeNavigationBarColor 사용 시 안드로이드 하단바 영역 이상해지는 문제로 수

--- App.tsx
+++ App.tsx
@@ -4,14 +4,12 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import DialogProvider from '@states/context/DialogProvider';
 import GlobalStateProvider from '@states/context/GlobalStateProvider';
 import { enableScreens } from 'react-native-screens';
-import changeNavigationBarColor from 'react-native-navigation-bar-color';
 import Navigation from './src/navigation';
 
 const App = (): React.JSX.Element => {
   const queryClient = new QueryClient();
 
   enableScreens();
-  changeNavigationBarColor('transparent');
 
   return (
     <QueryClientProvider client={queryClient}>
