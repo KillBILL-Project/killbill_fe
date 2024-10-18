0bb0eba88f88dbfe33f8623166196f8ac2a53bb7
bumja <gngk1234@naver.com>
2024-10-18 17:33:43 +0900

    feat: 위치 복사 시 토스트 생성되도록 수정

--- src/screens/home/TrashLocation/TrashCanAddress.tsx
+++ src/screens/home/TrashLocation/TrashCanAddress.tsx
@@ -9,9 +9,16 @@ import {
 } from '@screens/home/TrashLocation/TrashLocation.style';
 import { ITrashCanLocation } from '@services/api/trashService';
 import Clipboard from '@react-native-clipboard/clipboard';
+import { useSetRecoilState } from 'recoil';
+import { isShowToastState, toastMessageState } from '@states/notification';
 
 const TrashCanAddress = ({ selectedLocation }: { selectedLocation: ITrashCanLocation }) => {
+  const setToastMessage = useSetRecoilState(toastMessageState);
+  const setIsShowToast = useSetRecoilState(isShowToastState);
+
   const handleCopyButton = () => {
+    setToastMessage('주소가 복사되었습니다.');
+    setIsShowToast(true);
     Clipboard.setString(selectedLocation?.address ?? '');
   };
 
